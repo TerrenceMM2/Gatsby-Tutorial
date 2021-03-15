86620e46b622f0bab60350f500fba4d0ee35bfa5
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-03-15 16:32:16 -0500

    complete part 7 and 8 of tutorial - page data, build, seo

--- part-two/gatsby-config.js
+++ part-two/gatsby-config.js
@@ -6,9 +6,25 @@
 
 module.exports = {
     siteMetadata: {
-        title: `Pandas Eating Lots`,
+        title: `Gatsby Tutorial`,
+        description: `Gatsby Tutorial featuring Pandas.`,
+        author: `Terrence Mahnken`,
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `GatsbyJS`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#6b37bf`,
+                theme_color: `#6b37bf`,
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: `standalone`,
+                icon: `src/images/icon.png`, // This path is relative to the root of the site.
+            },
+        },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -18,6 +34,8 @@ module.exports = {
         },
         `gatsby-transformer-remark`,
         `gatsby-plugin-emotion`,
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-typography`,
             options: {
--- part-two/src/pages/pandas-eat-bamboo.md
+++ part-two/src/pages/pandas-eat-bamboo.md
@@ -5,4 +5,4 @@ date: "2021-03-14"
 
 Pandas eat ... snickers.
 
-<iframe width="560" height="315" src="https://www.youtube.com/watch?v=vgBjLyXqMAo" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vgBjLyXqMAo" frameborder="0" allowfullscreen></iframe>
